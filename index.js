fea1cad28d2c799cb6d9e8ffe10dd1d447579f58
sunil <sunilsharma21292@gmail.com>
2026-02-14 16:48:36 +0000

    gitignore pushed

---
+++ index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ status: 'Server is running', time: new Date() });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
